04167ac22dc858db6e88515ddc9a50db77ee9abc
falk <falk.schwiefert@gmail.com>
2019-04-03 18:32:40 +0200

    first commit

---
+++ src/reducers/contacts.js
@@ -0,0 +1,51 @@
+const initialState = [
+  {
+    _id: 1,
+    firstname: 'Falk',
+		lastname: 'Schwiefert',  
+		email: 'falk.schwiefert@gmail.com',
+		country: 'Spain',
+  },
+  {
+  _id: 2,
+  firstname: 'Test',
+	lastname: 'TestTest', 
+	email: 'test@gmail.com',
+	country: 'test',
+  },
+]
+
+const contacts = (state = initialState, action) => {
+
+	switch (action.type) {
+
+		case 'GET_CONTACTS':
+			return state 
+
+		case 'ADD_CONTACT':
+			return state.concat([{
+				_id: action.id, 
+				name: action.name, 
+				phone: action.phone, 
+				email: action.email
+			}])
+
+		case 'DELETE_CONTACT':
+			return state.filter( contact => {
+				return (contact._id !== action.id)
+			})
+
+		case 'UPDATE_CONTACT':
+			return state.map( contact => {
+				if (contact._id === action.id) {
+					return {_id: action.id, name: action.name, phone: action.phone, email: action.email}
+				}
+				return contact
+			})
+		default:
+			return state		
+
+	}
+}
+
+export default contacts
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import contacts from './contacts'
+
+const contactsApp = combineReducers({
+	contacts
+})
+
+export default contactsApp
