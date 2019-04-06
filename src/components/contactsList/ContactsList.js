542ebd824649c6611cef83aa39bf8c7623dc5b3d
falk <falk.schwiefert@gmail.com>
2019-04-06 19:33:03 +0200

    refactored container, broke redirect

--- src/components/contactsList/ContactsList.js
+++ src/components/contactsList/ContactsList.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { Component }from 'react'
 import { AddButton } from '../addButton/AddButton'
-import AllContacts from '../../containers/allContacts/AllContacts'
+import { DetailsButton } from'../../components/detailsButton/DetailsButton'
+import { DeleteButton } from '../../components/deleteButton/DeleteButton'
 import './contactsList.css'
 
+class ContactsList extends Component {
 
-export const ContactsList = () => (
-  <div className='contacts-list'>
-    <AddButton />
-    <AllContacts />
-  </div>
-)
+  deleteHandler(contact) {
+    this.props.deleteThisContact(contact)
+  }
+
+  createContactLine(contact) {
+    return(
+      <div className='contact-line' key={contact._id}>
+        <div className='contact-line_last-name'>{contact.lastName}</div>
+        <div className='contact-line_first-name'>{contact.firstName}</div>
+        <DetailsButton contact={contact}/>
+        <DeleteButton deleteContact={() => this.deleteHandler(contact)}/>
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div className='contacts-list'>
+        <AddButton />
+        {this.props.contacts.map(contact => this.createContactLine(contact))}
+      </div>
+    )
+  }  
+}
+
+export default ContactsList
